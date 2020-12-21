345f22ccaaa5306fdde7d959d2cf8bd30cd6690d
du-du-zi <jaehee_sg@naver.com>
2020-12-22 02:13:38 +0900

    DB 연동수정

--- app.py
+++ app.py
@@ -1,58 +1,67 @@
 from flask import Flask, jsonify, request
+import sqlite3
 
 app = Flask(__name__)
 
+#db_init
+table_name = "menus"
+conn = sqlite3.connect("test.db",check_same_thread=False)
+cur = conn.cursor()
+"""
 menus = [
     {"id":1,"name":"Espresso","price":3800},
     {"id":2,"name":"Americano","price":4100},
     {"id":3,"name":"CafeLatte","price":4600}
 ]
+cur.execute("CREATE TABLE "+ table_name +" (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(100) NOT NULL, price INTEGER NOT NULL)")
+for item in menus:
+    insert(cur,conn,item['name'],item['price'])
+"""
 
-#GET /menus | 자료를 가져온다
+def insert(name, price):
+    cur.execute(
+        "insert into " + table_name +" (name, price) values (" +  
+        "'" + name +  "'," +
+        str(price) + ")")
+    conn.commit()
+
+def select_all():
+    cur.execute("select * from " + table_name)
+    rows = cur.fetchall()
+    return rows
+
+def delete(id):
+    cur.execute(
+        "delete from " + table_name + " where id = " + str(id)
+    )
+    conn.commit()
+
+def update(id,name,price):
+    cur.execute(
+        "update " + table_name + " set name = '"+name+"', price = "+str(price)+" where id = "+str(id) 
+    )
+    conn.commit()
+
 @app.route('/menus') 
 def get_menus():
-    return jsonify({"menus":menus})
+    return jsonify(select_all())
 
-#POST /menus | 자료를 자원에 추가한다.
-#과제 - ID야 움직여라 얍
-next_id = len(menus) + 1
 @app.route('/menus', methods=['POST'])
-def create_menus(): #request가 JSON이라고 가정
-    # 전달받은 자료를 menus 자원에 추가
-    request_data = request.get_json() #{"name":...,}
-    global next_id
-    new_menu = {
-        "id" : next_id,
-        "name" : request_data['name'],
-        "price" : request_data['price'],
-    } # PST에서 딸려오는 데이터들을 payload라고도 함.
-    next_id += 1
-    menus.append(new_menu)
-    return jsonify(new_menu)
+def create_menus():
+    request_data = request.get_json()
+    insert(request_data['name'],request_data['price'])
+    return jsonify(select_all())
 
-#과제 - 메뉴 관리 CRUD 구현하기
 @app.route('/menus/<int:id>',methods=['PUT'])
 def update_menus(id):
     request_data = request.get_json()
-    for item in menus:
-        if item['id'] == id:
-            item['name'] = request_data['name']
-            item['price'] = request_data['price']
-            break
-    else:
-        return 'Data not exist'
-    return jsonify(menus)
+    update(id,request_data['name'],request_data['price']) 
+    return jsonify(select_all())
 
 @app.route('/menus/<int:id>',methods=['DELETE'])
 def delete_menus(id):
-    request_data = request.get_json()
-    for item in menus:
-        if item['id'] == id:
-            menus.remove(item)
-            break
-    else:
-        return 'Data not exist'
-    return jsonify(menus)
+    delete(id)
+    return jsonify(select_all())
 
 @app.route('/')
 def hello_flask():
