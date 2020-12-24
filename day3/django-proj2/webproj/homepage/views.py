2b9646ad3afbdf321baa7e9bfc97a0f8abf55217
ChaeLinYeo <evergreen786@naver.com>
2020-12-24 16:08:31 +0900

    view 수정

--- day3/django-proj2/webproj/homepage/views.py
+++ day3/django-proj2/webproj/homepage/views.py
@@ -5,5 +5,4 @@ from django.shortcuts import HttpResponse, render
 
 # Create your views here.
 def index(request):
-    nums = [1, 2, 3, 4, 5]
-    return render(request, 'index.html', {"my_list" : nums})
+    return render(request, 'index.html')
