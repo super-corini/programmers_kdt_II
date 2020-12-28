f279e19a79125c9172bdedf28967b49a79b0e553
Jinpyo Hong <hojp7874@gmail.com>
2020-12-29 06:39:11 +0900

    이미지 크롤링, 수정폼 js

--- day4/menuboard/urls.py
+++ day4/menuboard/urls.py
@@ -5,5 +5,6 @@ app_name = 'menuboard'
 urlpatterns = [
     path('', views.profile, name="profile"),
     path('fruits/', views.fruit_create_read, name="fruit_create_read"),
-    path('fruits/<int:pk>', views.fruit_update_delete, name="fruit_update_delete"),
+    path('fruits/<int:pk>/update/', views.fruit_update, name="fruit_update"),
+    path('fruits/<int:pk>/delete/', views.fruit_delete, name="fruit_delete"),
 ]
--- day4/menuboard/views.py
+++ day4/menuboard/views.py
@@ -1,8 +1,9 @@
+from django.http.response import JsonResponse
 from django.shortcuts import render, redirect
 from .models import Fruit
 from .forms import FruitForm
 from django.shortcuts import get_object_or_404
-from django.views.decorators.http import require_http_methods, require_GET
+from django.views.decorators.http import require_http_methods, require_GET, require_POST
 
 # crawling
 from bs4 import BeautifulSoup
@@ -41,14 +42,27 @@ def fruit_create_read(request):
     return render(request, 'menuboard/fruitlist.html', context)
 
 
-# Update, Delete
-# @require_http_methods(['PUT', 'DELETE'])
-def fruit_update_delete(request, pk):
+# Update
+@require_POST
+def fruit_update(request, pk):
     fruit = get_object_or_404(Fruit, pk=pk)
-    # Update
-    if request.method == "PUT":
-        pass
-    else:
-        fruit.delete()
-        print()
-        return redirect('menuboard:fruit_create_read')
+    print(request.POST)
+    name = quote(request.POST["name"])
+    response = urlopen(f'https://terms.naver.com/search.nhn?query={name}')
+    soup = BeautifulSoup(response, 'html.parser')
+    request_dict = request.POST.dict()
+    request_dict['imgsrc'] = soup.select("div.thumb_area img")[0]["data-src"]
+    form = FruitForm(request_dict, instance=fruit)
+    if form.is_valid():
+        form.save()
+        form_dict = form.dict()
+        print(form_dict)
+        return JsonResponse(form_dict)
+
+
+# Delete
+@require_POST
+def fruit_delete(request, pk):
+    fruit = get_object_or_404(Fruit, pk=pk)
+    fruit.delete()
+    return redirect('menuboard:fruit_create_read')
