b1d10a841e5270b15301e7690ca00323bbd3ef7e
Kyubum Shin <kbum0617@naver.com>
2021-05-07 08:51:18 +0900

    day1 필수과제
    implement put delete

---
+++ day1/app.py
@@ -0,0 +1,56 @@
+from flask import Flask, jsonify, request
+
+
+app = Flask(__name__)
+
+
+menus = [
+    {"id":1, "name":"Espresso","price":3800},
+    {"id":2, "name":"Americano","price":4100},
+    {"id":3, "name":"CafeLatte","price":4600}
+]
+
+@app.route('/')
+def hello_flask():
+    return "Hello World!"
+
+# Get /menus
+@app.route('/menus')
+def get_menus():
+    return jsonify({"menus":menus})
+
+# POST /menus
+@app.route('/menus', methods=['POST'])
+def create_menu():
+    # 전달받은 자료를 menus 자원에 추가
+    request_data = request.get_json()
+    new_menu = {
+        "id" : 4,
+        "name" : request_data['name'],
+        "price" : request_data['price']
+    }
+    menus.append(new_menu)
+    return jsonify(new_menu)
+
+#PUT /menus
+@app.route('/menus/<_id>', methods=['PUT'])
+def change_menu(_id):
+    request_data = request.get_json()
+    for i in range(len(menus)):
+        if menus[i]['id'] == int(_id):
+            menus[i]['name'] = request_data['name']
+            menus[i]['price'] = request_data['price']
+            break
+    return jsonify({"menus":menus})
+
+#DEL /menus
+@app.route('/menus/<_id>', methods=['DELETE'])
+def del_menu(_id):
+    for i in range(len(menus)):
+        if menus[i]["id"] == int(_id):
+            a = menus[i]
+            del menus[i]
+            return a
+
+if __name__ == "__main__":
+    app.run()
