7e97e6f74618734217f491bfb192bf6a5b4d73f0
taehyun-learn <chirs5207@naver.com>
2021-05-10 14:54:13 +0900

    week4 day1 mission

--- day1/flask/config.py
+++ day1/flask/config.py
@@ -2,8 +2,8 @@ db = {
     'user'     : 'root',		
     'password' : '0000',		
     'host'     : 'localhost',	
-    'port'     : 5000,			
-    'database' : 'minitter'		
+    'port'     : 3306,			
+    'database' : 'menus'		
 }
 
 DB_URL = f"mysql+mysqlconnector://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['database']}?charset=utf8"
