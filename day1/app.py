0870760618f69577386bc4f5af87d88e995d349c
nodenode1 <il6642@naver.com>
2021-05-11 01:21:37 +0900

    DB 구현

--- day1/app.py
+++ day1/app.py
@@ -1,52 +1,74 @@
 from flask import Flask,jsonify,request
+import sqlite3
+
+conn=sqlite3.connect('test.db')
+
+c=conn.cursor()
+
+c.execute("CREATE TABLE cafe(id integer,name varchar(50),price integer)")
+
+c.execute("INSERT INTO cafe VALUES(1,'Espresso', 3000)")
+
+c.execute("INSERT INTO cafe VALUES(2,'ICE Espresso', 4000)")
+
+c.execute("INSERT INTO cafe VALUES(3,'Latte', 3500)")
 
 
+conn.commit()
 app=Flask(__name__)
 
-menus=[
-    {"id":1 , "name":"Espresso","price": 38000},
-    {"id":2 , "name":"Americano","price": 38000},
-    {"id":3 , "name":"Cafe Latte","price": 38000},
-]
+
 
 @app.route('/')
 def hello_flask():
+
     return "Hello World"
 
-@app.route('/menus')
+@app.route('/menus',methods=['GET'])
 def get_menus():
-    return jsonify({"menus":menus})
+    con = sqlite3.connect('test.db')
+    c = con.cursor()
+    c.execute('SELECT * FROM cafe')
+    a = c.fetchall()
+    con.commit()
+    con.close()
+    return jsonify({"menus":a})
 
 @app.route('/menus', methods=['POST'])
 def create_menus():
     request_data=request.get_json()
-    new_menu={
-        "id":len(menus)+1,
-        "name":request_data['name'],
-        "price":request_data['price']
-    }
-    menus.append(new_menu)
-    return jsonify(new_menu)
+    con = sqlite3.connect('test.db')
+    c = con.cursor()
+    c.execute('SELECT COUNT(*) FROM cafe')
+    id,name,price=c.fetchone()[0]+1,request_data['name'],request_data['price']
+    c.execute('INSERT INTO cafe (id,name,price) VALUES (?,?,?)',(id,name,price))
+    c.execute('SELECT * FROM cafe')
+    m=c.fetchall()
+    con.commit()
+    con.close()
+
+    return jsonify({"menus":m})
 
 @app.route('/menus/<int:id>',methods=['PUT'])
 def update_menus(id):
     request_data=request.get_json()
-    for menu in menus:
-        if menu["id"] == id:
-            menu["name"]=request_data["name"]
-            menu["price"]=request_data["price"]
-            return jsonify(menus)
-    return menus
-
+    con = sqlite3.connect('test.db')
+    c = con.cursor()
+    c.execute('UPDATE cafe SET name=?,price=? WHERE id=?',(request_data['name'],request_data['price'],id))
+    c.execute('SELECT * FROM cafe')
+    m=c.fetchall()
+    con.commit()
+    con.close()
+    return jsonify({"menus":m})
 @app.route('/menus/<int:id>',methods=['DELETE'])
 def delete_menus(id):
-
-    for menu in menus:
-        if menu["id"] == id:
-            del menus[menus.index(menu)]
-            return jsonify(menus)
-    return menus
-
-
+    con = sqlite3.connect('test.db')
+    c = con.cursor()
+    c.execute("DELETE FROM cafe WHERE id= :ID",{"ID":id})
+    c.execute('SELECT * FROM cafe')
+    m=c.fetchall()
+    con.commit()
+    con.close()
+    return jsonify({"menus":m})
 if __name__=='__main__':
     app.run()
