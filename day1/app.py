a34028f8236efe42b6e4c7773d83bec0e9c60d95
sangin_Park <sangin0829@gmail.com>
2021-05-10 18:38:27 +0900

    보너스 과제 2.py

--- day1/app.py
+++ day1/app.py
@@ -1,47 +1,67 @@
 from flask import Flask,jsonify, request
+import pymysql
 
+''' local 환 경 용 세 팅 
+db = pymysql.connect(host="localhost", user="root", passwd="*****", db="flask_test", charset="utf8")
+cur = db.cursor()
 app = Flask(__name__)
-
-menus = [
-    {"id":1, "name":"Espresso", "price": 4800},
-    {"id":2, "name":"Americano", "price": 4100},
-    {"id":3, "name":"CafeLatte", "price": 4600},
-]
-
+sql = "SELECT * from menus"
+cur.execute(sql)
+data_list = cur.fetchall()
+'''
 
 @app.route('/')
 def hello_flask():
-    return "Hello World"
+    return "Bonus_2 과제 구현을 위한 Homepage"
 
 # Get /menus / 자료를 가져온다.
 @app.route('/menus')
 def get_menus():
-    return jsonify({"menus":menus})
+    cur = db.cursor()
+    sql = "SELECT * from menus"
+    cur.execute(sql)
+    data_list = cur.fetchall()
+    return jsonify({"menus":data_list})
 
 # Post
 @app.route('/menus', methods = ['POST'])
 def create_menu():
+    global data_list
+    cur = db.cursor()
     #전달받은 자료를 메뉴에 추가
     request_data = request.get_json()
-    new_menu = {
-        "id": len(menus)+1,
-        "name": request_data['name'],
-        "price": request_data['price'],
-    }
-    menus.append(new_menu)
-    return jsonify(new_menu)
+    sql_post ="INSERT INTO menus (id, name, price) VALUES (%s, %s, %s);"
+    cur.execute(sql_post,(len(data_list)+1,request_data['name'],request_data['price']))
+    cur.execute("SELECT * from menus")
+    data_list=cur.fetchall()
+    db.commit()
+    return jsonify({"menus":data_list})
+
 
 @app.route('/menus/<int:id>', methods = ['PUT'])
 def update_id(id):
+    global data_list
+    cur = db.cursor()
     request_data = request.get_json()
-    menus[id-1]['name'] = request_data['name']
-    menus[id-1]['price'] = request_data['price']
-    return jsonify({"menus":menus})
+    name,price = request_data['name'],request_data['price']
+    sql_update = 'UPDATE menus SET name = %s, price = %s WHERE id = %s;'
+    cur.execute(sql_update,(name,price,id))
+    cur.execute("SELECT * from menus")
+    db.commit()
+    data_list = cur.fetchall()
+    return jsonify({"menus":data_list})
+
 # DELETE
 @app.route('/menus/<int:id>', methods=['DELETE'])
 def delete_menu(id):
-    del menus[id-1]
-    return jsonify({'menu': menus})
+    global data_list
+    cur = db.cursor()
+    sql_delete = 'DELETE From menus WHERE id = %s'
+    cur.execute(sql_delete,id)
+    cur.execute("SELECT * from menus")
+    data_list = cur.fetchall()
+    db.commit()
+    return jsonify({'menu': data_list})
 
 if __name__ == '__main__':
     app.run()
