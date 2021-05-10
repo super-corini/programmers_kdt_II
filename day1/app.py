20bbb65435ecd83dd62e9d21e980b5d4e0c24b5e
illstandtall <illstandtall@gmail.com>
2021-05-11 06:59:28 +0900

    week4 day1 (4): 보너스 과제 II : 데이터베이스 연동하기

--- day1/app.py
+++ day1/app.py
@@ -1,68 +1,46 @@
 from flask import Flask, jsonify, request
+from models import db, Menu
 
-# Flask app의 이름
+
 app = Flask(__name__)
 
-menus = [
-    {"id": 1, "name": "Esspresso", "price": 3800},
-    {"id": 2, "name": "Americano", "price": 4200},
-    {"id": 3, "name": "CafeLatte", "price": 4600}
-]
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./menu.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+db.app = app
+db.create_all()
 
-# 주소 '/'(root) 를 만나면 아래의 함수를 실행
+
 @app.route('/')
-def hello_falsk():
-    return "Hello World!!!!!!"
+def hello_flask():
+    return 'hello flask:)'
+
 
 # GET
-@app.route('/menus')
+@app.route('/menus') 
 def get_menus():
-    return jsonify({"menus": menus})
+    return jsonify(Menu.get_menus())
 
+    
 # POST
 @app.route('/menus', methods=['POST'])
 def create_menu():
     request_data = request.get_json()
-    new_menu = {
-        "id": len(menus)+1,
-        "name": request_data['name'],
-        "price": request_data['price']
-    }
-    menus.append(new_menu)
-    return jsonify(new_menu)
+    return jsonify(Menu.add_menu(request_data["name"], request_data["price"]))
 
 
 # PUT
 @app.route('/menus/<int:id>', methods=['PUT'])
 def update_menu(id):
     request_data = request.get_json()
-    for menu in menus:
-        if menu["id"] == id:
-            tmp = menu
-            menu["name"] = request_data['name']
-            menu["price"] = request_data['price']
-            break
-    else:
-        return "찾으시는 id를 가진 메뉴가 없습니다."
-
-    return jsonify({"menus": menus})
+    return jsonify(Menu.update_menu(id, request_data["name"], request_data["price"]))
 
 
 # DELETE
 @app.route('/menus/<int:id>', methods=['DELETE'])
 def delete_menu(id):
-    for i in range(len(menus)):
-        if menus[i]["id"] == id:
-            for j in range(i+1, len(menus)):
-                menus[j]["id"] = j
-            menus.pop(i)
-            break
-    else:
-        return "찾으시는 id를 가진 메뉴가 없습니다."
-
-    return jsonify({"menus": menus})
+    return jsonify(Menu.delete_menu(id))
 
 
-# 처음 실행한(main) 함수의 경우 현재 app을 실행하라는 의미
 if __name__ == '__main__':
     app.run(debug=True)
