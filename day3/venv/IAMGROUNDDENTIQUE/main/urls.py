a5c3fa955bcda6a72725c4e7597e9bb91ec447c0
ohtjqkd <ohtjqkd@gmail.com>
2021-05-12 22:34:00 +0900

    simplest website

---
+++ day3/venv/IAMGROUNDDENTIQUE/main/urls.py
@@ -0,0 +1,11 @@
+# main 서브앱의 urls
+# 서브앱의 urls는 같은 위치의 view.py의 함수로 연결을 담당 (path)
+# 같은 위치의 view.py를 식별 못하면 import
+
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index')
+]
