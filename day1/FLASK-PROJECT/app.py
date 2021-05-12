072dcc29b8d7aadd186c4e1f2f0621b1a163475f
Kim Jeonghyun <kimjeonghyun.jkim@gmail.com>
2021-05-12 22:57:37 +0900

    refactor app.py and delete db.aqlite, model.py
    필수 과제와 추가 과제 1만을 수행하는 app.py로 수정합니다.
    추가 과제 2를 수행하는 프로젝트 폴더를 따로 제작하여 제출할 예정입니다.

--- day1/FLASK-PROJECT/app.py
+++ day1/FLASK-PROJECT/app.py
@@ -71,26 +71,6 @@ def delete_menus(id):
         print(e)
         return jsonify({'error': "해당 자료가 존재하지 않습니다."})
 
-# 현재있는 파일의 디렉토리 절대경로
-basdir = os.path.abspath(os.path.dirname(__file__))
-# basdir 경로안에 DB파일 만들기
-dbfile = os.path.join(basdir, 'db.sqlite')
-
-# SQLAlchemy 설정
-
-# 내가 사용 할 DB URI
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
-# 비지니스 로직이 끝날때 Commit 실행(DB반영)
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-# 수정사항에 대한 TRACK
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# SECRET_KEY
-app.config['SECRET_KEY'] = 'jqiowejrojzxcovnklqnweiorjqwoijroi'
-
-db.init_app(app)
-db.app = app
-db.create_all()
-
 
 if __name__ == "__main__":
     app.run(debug=True)
