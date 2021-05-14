0bd992fb5add0cca83842e1511062c260497901a
hwangwoojin <hwangwoojin.dev@gmail.com>
2021-05-14 23:00:46 +0900

    [Day 3] 보너스 과제 : ⭐️아이😊엠 그라운드⛳️ 자기💁‍♀️소개💁‍♂️ 하기⭐️

--- day3/webproj/webproj/urls.py
+++ day3/webproj/webproj/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from homepage.views import index as homepage_index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage_index),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
