b2416b8b3f73053431ba7deeafc1aba622a95d32
lynne <kimahlynne99@gmail.com>
2021-05-17 05:24:20 +0900

    d

--- day1/models.py
+++
@@ -1,7 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy()
-
-class Menus(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(32))
-    price = db.Column(db.Integer)
