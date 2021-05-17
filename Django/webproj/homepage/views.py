1787e585add1160edc9cfcff8edcaaa7f6a9e626
Kyubum Shin <kbum0617@naver.com>
2021-05-17 23:44:57 +0900

    [Week5] 신규범
    Day1 과제

---
+++ Django/webproj/homepage/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, HttpResponse
+
+
+# Create your views here.
+def index(request):
+    #HttpResponse("Hello World")
+    my_name = "Kyubum Shin"
+    return render(request,'index.html',{"my_name" : my_name})
