5e3d7552e988dc6a07d32d85652869446179e792
DongIk-Jang <jdandy14@gmail.com>
2021-05-17 15:00:45 +0900

    [week5-day1,2] Django 필수과제와 보너스과제 통합제출합니다.

---
+++ Django-Mission/django-proj/webproj/webproj/urls.py
@@ -0,0 +1,30 @@
+"""webproj URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path
+from homepage.views import index, introduction, coffee_view, coffee_stock, coffee_post, coffee_put, coffee_delete
+
+urlpatterns = [
+    path('', index), # 127.0.0.1/
+    path('GET/', introduction), # 127.0.0.1/GET/
+    path('coffee/', coffee_view), # 127.0.0.1/coffee/
+    path('GET/coffees/', coffee_stock),
+    path('POST/coffees/', coffee_post),
+    path('PUT/coffees/<str:pk>', coffee_put),
+    path('DELETE/coffees/<str:pk>', coffee_delete),
+    path('admin/', admin.site.urls), # 127/0.0.1/admin/
+]
