fd71619b40201a115f440dc5751866a3ddc8de4e
spongebob03 <bmofinnjake08@gmail.com>
2021-05-18 15:12:44 +0900

    강의 실습) django form

---
+++ week5/web_porj/intro/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Coffee # Model 호출
+
+class CoffeeForm(forms.ModelForm): # ModelForm을 상속받는 CoffeeForm 생성
+    class Meta:
+        # 폼을 만들기 위해 어떤 모델을 써야하는지 지정
+        model = Coffee
+        fields = ('name', 'price', 'is_ice')
--- week5/web_porj/intro/views.py
+++ week5/web_porj/intro/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import HttpResponse, render
 from .models import Coffee
+from .forms import CoffeeForm
 
 # Create your views here.
 def index(request):
@@ -9,4 +10,11 @@ def index(request):
 
 def coffee_view(request):
     coffee_all = Coffee.objects.all() # select * from Coffee;
-    return render(request, 'coffee.html', {"coffee_list": coffee_all})
+    
+    if request.method == "POST":
+        form = CoffeeForm(request.POST) # POST를 바탕으로 Form을 완성
+        if form.is_valid():
+            form.save()                 # Form이 유효하면 -> 저장
+
+    form = CoffeeForm()
+    return render(request, 'coffee.html', {"coffee_list": coffee_all, "coffee_form":form})
