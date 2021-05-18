bf171218fb74ce116a7cf8c634b3addc084ce73c
heejuhee <ujst02@gmail.com>
2021-05-18 19:17:47 +0900

    week5_day1_core

---
+++ week5_day1/webproj/homepage/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import HttpResponse, render
+
+# Create your views here.
+def index(request):
+    nums = [1,2,3,4,5]
+    return render(request, 'index.html', {"my_list": nums})
