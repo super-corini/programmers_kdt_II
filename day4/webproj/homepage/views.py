1293bf1fe57afd477b89bd7cdbf243ffb3ea0833
marquis08 <roal214@naver.com>
2021-05-19 17:46:28 +0900

    init: base webproj with homepage app with basic introduction home html view

---
+++ day4/webproj/homepage/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    myinfo = {}
+    myinfo['name'] = 'YILGUK SEO'
+    myinfo['major'] = 'Business Administration'
+    myinfo['interest'] = 'AI'
+    return render(request, 'index.html', {'myinfo':myinfo}) # Must add template dir in settings.py
