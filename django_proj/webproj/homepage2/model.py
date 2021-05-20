829a5ed3d54d7d92b256d98876333fc1f67b8b99
changhyeonlee-0703 <changhyeonlee9473@gmail.com>
2021-05-20 16:41:09 +0900

    add model.py and template

--- django_proj/webproj/homepage2/model.py
+++ django_proj/webproj/homepage2/model.py
@@ -8,9 +8,8 @@ class BugerMaterial(models.Model):
         return self.type
 
     type = models.CharField(default="", max_length=30)
-    #brend_name = models.CharField(default="",null=True, max_length=30)
-    #director_name = models.CharField(default="",null=True, max_length=30)
-    #price = models.IntegerField(default=0,null=True,)
+    brend_name = models.CharField(default="",null=True, max_length=30)
+    director_name = models.CharField(default="",null=True, max_length=30)
+    price = models.IntegerField(default=0,null=True)
     stock = models.IntegerField(default=0)
     is_set = models.BooleanField(default=False)
-
