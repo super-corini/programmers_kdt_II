ef224d7fcd6e6dac8c56ced0a44f1a9607a7bd56
Kim Jeonghyun <kimjeonghyun.jkim@gmail.com>
2021-05-20 19:12:57 +0900

    update Day2 project
    secret_key 보안 문제 개선
    .git_ingnore 추가

--- Week5/Day2/webproj/webproj/settings.py
+++ Week5/Day2/webproj/webproj/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$%dp+#-7#rge_$wqk(*&@c@m7(dss6x@y1ahe*4qvrau)4$-7e'
+import os
+
+SECRET_KEY = os.environ['DJANGO_KEY']
+# SECRET_KEY = 'test'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
