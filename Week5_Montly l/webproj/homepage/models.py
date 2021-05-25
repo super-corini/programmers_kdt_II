2990cd90aee6913c0e7243d6b3b6b1fda1c8d340
Junyup Lee <76204590+junyup2@users.noreply.github.com>
2021-05-26 05:30:01 +0900

    Monthly Project I
    - Mandatory Project 제출
     - Pokemon EDA
     - `PythonAnywhere` 를 이용하여 배포
    - Bonus Part 도 해보려 했으나 Bootstrap 적용 방법이 익숙하지 않아 Mandatory만 제출하고 추후에 해볼 예정.

---
+++ Week5_Montly l/webproj/homepage/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+"""
+    문자열 : CharField
+    숫자 : IntegerField, SmallIntegerField, ...
+    논리형 : BooleanField
+    시간/날짜 : DateTimeField
+    ...
+"""
