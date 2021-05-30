e8f994d751f356e5e80313d7fd5edc2ecee781be
metterian <dzzy6505@gmail.com>
2021-05-30 18:56:10 +0900

    add: django initiation

---
+++ day4/supply_mangement/supply_mangement/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for supply_mangement project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'supply_mangement.settings')
+
+application = get_asgi_application()
